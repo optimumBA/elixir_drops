e53063e0bd2ea0661582df4eb36491b58cd5c86e
kagure-nyakio <kagurenyakio@gmail.com>
2024-07-26 13:26:24 +0300

    Make drop fields non-nullable

--- priv/repo/migrations/20240719103543_create_drops.exs
+++ priv/repo/migrations/20240719103543_create_drops.exs
@@ -4,8 +4,8 @@ defmodule ElixirDrops.Repo.Migrations.CreateDrops do
   def change do
     create table(:drops, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :body, :text
-      add :title, :string
+      add :body, :text, null: false
+      add :title, :string, null: false
       add :user_id, references(:users, on_delete: :delete_all, type: :binary_id)
 
       timestamps()
