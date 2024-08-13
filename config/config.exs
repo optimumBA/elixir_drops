4742256d021fece15f2bfbab2d27c7445340e756
kagure-nyakio <kagurenyakio@gmail.com>
2024-08-13 11:47:04 +0300

    Maybe override github redirect

--- config/config.exs
+++ config/config.exs
@@ -77,7 +77,7 @@ config :ueberauth, Ueberauth,
       {Ueberauth.Strategy.Github,
        [
          default_scope: "read:user,user:email",
-         send_redirect_uri: false
+         redirect_uri: "https://elixirdrops-pr-2.fly.dev/"
        ]}
   ]
 
