20f00607746c5e70afdd5542ba937693e3c7efee
kagure-nyakio <kagurenyakio@gmail.com>
2024-09-06 12:13:15 +0300

    Add tests for XSS attacks

--- test/elixir_drops_web/live/drop_live_test.exs
+++ test/elixir_drops_web/live/drop_live_test.exs
@@ -129,5 +129,22 @@ defmodule ElixirDropsWeb.DropLiveTest do
       assert {:error, {:live_redirect, %{to: ^path}}} =
                live(conn, ~p"/drops/#{drop_id}")
     end
+
+    test "Javascript scripts inside the drop is not executed", %{conn: conn, user: user} do
+      drop =
+        drop_fixture(
+          %Drop{},
+          user,
+          %{
+            body:
+              "dfdf\n\n```js\n<script>\nlet header = document.querySelector('header')\n\nconst tempDiv = document.createElement(\"div\");\ntempDiv.textContent = \"Some malicious code\";\n\nheader.insertAdjacentElement('afterend', tempDiv);\n</script>\n```",
+            title: "Drop with script"
+          }
+        )
+
+      {:ok, _live, html} = live(conn, ~p"/drops/#{drop.id}")
+
+      refute html =~ ~r|<div>"Some malicious code"</div>|
+    end
   end
 end
