3f818afda10bcb6ce36423fb53b33e7eca4a7f2a
kagure-nyakio <kagurenyakio@gmail.com>
2024-09-09 21:15:14 +0300

    code clean up

--- test/elixir_drops/workers/image_creation_worker_test.exs
+++ test/elixir_drops/workers/image_creation_worker_test.exs
@@ -11,7 +11,6 @@ defmodule ElixirDrops.Workers.ImageCreationWorkerTest do
   alias ElixirDrops.Workers.ImageCreationWorker
 
   setup :set_mox_global
-  setup :set_mox_global
   setup :verify_on_exit!
 
   defp drop_setup(_attrs) do
