a6d07a3ceee09f4be7b693e5a4a10f992547a95f
mbashia <mbashiavictor@gmail.com>
2024-10-22 07:46:16 +0300

    Handle when no code block is found

--- lib/elixir_drops_web/controllers/screenshot_generator_controller.ex
+++ lib/elixir_drops_web/controllers/screenshot_generator_controller.ex
@@ -14,6 +14,9 @@ defmodule ElixirDropsWeb.ScreenshotGeneratorController do
     with %Drop{} = drop <- Drops.get_drop(%{drop_id: id}),
          [code_block] <- Regex.run(@markdown_regex, drop.body, capture: :first) do
       render(conn, :index, code_block: code_block, layout: false)
+    else
+      _no_code_block ->
+        render(conn, :index, code_block: "No code block found", layout: false)
     end
   end
 end
