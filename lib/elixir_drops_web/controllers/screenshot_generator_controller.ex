3e1ec88a804c9f3be2676f484c9db4686704be86
mbashia <mbashiavictor@gmail.com>
2024-10-31 06:58:55 +0300

    Add wallaby auth logic to screenshot_Generator_controller

--- lib/elixir_drops_web/controllers/screenshot_generator_controller.ex
+++ lib/elixir_drops_web/controllers/screenshot_generator_controller.ex
@@ -3,6 +3,7 @@ defmodule ElixirDropsWeb.ScreenshotGeneratorController do
 
   alias ElixirDrops.Drops
   alias ElixirDrops.Drops.Drop
+  require Logger
 
   @type conn :: Plug.Conn.t()
   @type params :: map()
@@ -11,11 +12,31 @@ defmodule ElixirDropsWeb.ScreenshotGeneratorController do
 
   @spec index(conn(), params()) :: conn()
   def index(conn, %{"id" => id}) do
-    drop = Drops.get_drop(%{drop_id: id})
+    case authenticate(conn) do
+      :ok ->
+        handle_drop_retrieval(conn, id)
 
-    case drop do
-      %Drop{} ->
-        case Regex.run(@markdown_regex, drop.body, capture: :first) do
+      :error ->
+        request_auth(conn)
+    end
+  end
+
+  defp authenticate(conn) do
+    [username: username, password: password] = Application.get_env(:elixir_drops, :wallaby_auth)
+
+    with {request_username, request_password} <- Plug.BasicAuth.parse_basic_auth(conn),
+         true <- valid_username?(username, request_username),
+         true <- valid_password?(password, request_password) do
+      :ok
+    else
+      _invalid_auth -> :error
+    end
+  end
+
+  defp handle_drop_retrieval(conn, id) do
+    case Drops.get_drop(%{drop_id: id}) do
+      %Drop{body: body} ->
+        case Regex.run(@markdown_regex, body, capture: :first) do
           [code_block] ->
             render(conn, :index, code_block: code_block, layout: false)
 
@@ -27,4 +48,16 @@ defmodule ElixirDropsWeb.ScreenshotGeneratorController do
         send_resp(conn, :not_found, "404 Not Found")
     end
   end
+
+  defp request_auth(conn) do
+    conn
+    |> Plug.BasicAuth.request_basic_auth()
+    |> halt()
+  end
+
+  defp valid_username?(username, request_username),
+    do: Plug.Crypto.secure_compare(username, request_username)
+
+  defp valid_password?(password, request_password),
+    do: Plug.Crypto.secure_compare(password, request_password)
 end
