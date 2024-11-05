8b6121e95ef43a02e42590e140b84818402cddab
mbashia <mbashiavictor@gmail.com>
2024-11-05 12:04:54 +0300

    Upgrade Flame version

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule ElixirDrops.MixProject do
   defp app_deps do
     [
       {:autumn, "~> 0.2"},
-      {:flame, "~> 0.4.4"},
+      {:flame, "~> 0.5.1"},
       {:mdex, "~> 0.1"},
       {:mox, "~> 1.1", only: :test},
       {:oban, "~> 2.18"},
