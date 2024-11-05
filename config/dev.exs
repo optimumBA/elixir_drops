1661ff25eaadda8d7659f06e7c8bb353530c7bd1
mbashia <mbashiavictor@gmail.com>
2024-11-05 07:11:59 +0300

    Edit elixir drops wallaby config

--- config/dev.exs
+++ config/dev.exs
@@ -143,7 +143,7 @@ config :elixir_drops, :s3,
   region: System.get_env("AWS_REGION"),
   secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY")
 
-config :elixir_drops_wallaby,
+config :elixir_drops,
   wallaby_auth: [
     username: "elixir_drops_wallaby",
     password: "l3AVAovk4B8g5Sbq"
