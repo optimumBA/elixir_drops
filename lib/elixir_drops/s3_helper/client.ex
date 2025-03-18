7be894ddcbd4601eddb268f93010401a0d8350ac
Purity Gwaro <puritygwaro@gmail.com>
2025-03-18 20:02:40 +0300

    update client.ex file

--- lib/elixir_drops/s3_helper/client.ex
+++ lib/elixir_drops/s3_helper/client.ex
@@ -7,14 +7,13 @@ defmodule ElixirDrops.S3Helper.Client do
   @type drop :: Drop.t()
   @type filename :: binary()
   @type image :: binary()
-  @type screenshot_type :: :meta | :internal
-  @type content_type :: binary()
+  @type type :: binary()
   @type url :: binary()
 
-  @callback get_image(drop(), screenshot_type()) :: {:ok, url()} | {:error, any()}
-  @callback upload_image(image(), filename(), content_type()) :: {:ok, url()} | {:error, any()}
+  @callback get_image(drop(), atom()) :: {:ok, url()} | {:error, any()}
+  @callback upload_image(image(), filename(), type()) :: {:ok, url()} | {:error, any()}
 
-  @spec upload_image(image(), filename(), content_type()) :: {:ok, url()} | {:error, any()}
+  @spec upload_image(image(), filename(), type()) :: {:ok, url()} | {:error, any()}
   def upload_image(image, filename, type) do
     impl().upload_image(
       image,
@@ -23,7 +22,7 @@ defmodule ElixirDrops.S3Helper.Client do
     )
   end
 
-  @spec get_image(drop(), screenshot_type()) :: {:ok, url()} | {:error, any()}
+  @spec get_image(drop(), atom()) :: {:ok, url()} | {:error, any()}
   def get_image(drop, type), do: impl().get_image(drop, type)
 
   defp impl, do: Application.get_env(:elixir_drops, :s3_helper, Http)
