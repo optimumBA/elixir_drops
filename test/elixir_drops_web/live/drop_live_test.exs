de483295cb7c40def715d551442b924e395f5764
Dean Kinyua <kinyuadean@gmail.com>
2025-04-03 16:58:52 +0300

    write 2 test to assert that not logged in users cannot edit drops and only drop author can edit it

--- test/elixir_drops_web/live/drop_live_test.exs
+++ test/elixir_drops_web/live/drop_live_test.exs
@@ -23,9 +23,10 @@ defmodule ElixirDropsWeb.DropLiveTest do
       )
 
     user = user_fixture()
+    user2 = user_fixture_2()
     drop = drop_fixture(user)
 
-    %{conn: conn, drop: drop, user: user}
+    %{conn: conn, drop: drop, user: user, user2: user2}
   end
 
   describe "/" do
@@ -95,6 +96,28 @@ defmodule ElixirDropsWeb.DropLiveTest do
       {path, _flash} = assert_redirect(live)
 
       assert path == ~p"/d/#{drop.short_id}"
+    end
+
+    test "unathorized users cannot edit drops", %{conn: conn} do
+      {:ok, _live, html} = live(conn, ~p"/")
+
+      refute html =~ "Edit drop"
+    end
+
+    test "only a user who authored a drop can edit it", %{
+      conn: conn,
+      user: user,
+      user2: user2
+    } do
+      conn = sign_in_user(conn, user)
+      {:ok, _live, html} = live(conn, ~p"/")
+
+      assert html =~ "Edit drop"
+
+      conn2 = sign_in_user(conn, user2)
+      {:ok, _live, html2} = live(conn2, ~p"/")
+
+      refute html2 =~ "Edit drop"
     end
 
     test "gets updated with new drops", %{conn: conn, user: user} do
