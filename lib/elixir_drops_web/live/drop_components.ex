2437a92d4e3113b6716c104eeba8229d01ef7368
Dean Kinyua <kinyuadean@gmail.com>
2025-04-03 18:25:59 +0300

    order assigns alphabetically

--- lib/elixir_drops_web/live/drop_components.ex
+++ lib/elixir_drops_web/live/drop_components.ex
@@ -103,7 +103,7 @@ defmodule ElixirDropsWeb.DropComponents do
       </div>
 
       <.drop_card_menu
-      drop_user_id={@drop.user_id}
+        drop_user_id={@drop.user_id}
         id={@drop.id}
         short_id={@drop.short_id}
         user_id={@user_id}
