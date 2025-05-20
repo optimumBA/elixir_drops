ae9411f9a5e0958fc7c998509b626c81eeec8c95
Almir Sarajčić <almir.sarajcic@icloud.com>
2025-05-20 15:46:19 +0200

    Simplify migration

--- priv/repo/migrations/20250424090301_update_drop_nil_screenshot_columns.exs
+++ priv/repo/migrations/20250424090301_update_drop_nil_screenshot_columns.exs
@@ -2,10 +2,12 @@ defmodule ElixirDrops.Repo.Migrations.UpdateDropNilScreenshotColumns do
   use Ecto.Migration
 
   def up do
-    execute "UPDATE drops SET screenshot = jsonb_build_object('status', 'skipped', 'url', null) WHERE screenshot IS NULL"
+    execute ~S"""
+    UPDATE drops SET screenshot = '{"status": "skipped", "url": null}' WHERE screenshot IS NULL
+    """
   end
 
   def down do
-    execute "UPDATE drops SET screenshot = NULL WHERE screenshot = jsonb_build_object('status', 'skipped', 'url', null)"
+    execute "UPDATE drops SET screenshot = NULL WHERE screenshot->>'status' = 'skipped'"
   end
 end
