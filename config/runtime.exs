56e05ca82df34e5bede632622f5b91cbe7e09dfa
Dean Kinyua <kinyuadean@gmail.com>
2025-10-09 12:34:27 +0300

    fix github workflows

--- config/runtime.exs
+++ config/runtime.exs
@@ -212,11 +212,11 @@ if config_env() == :prod do
     region: aws_region,
     secret_access_key: aws_secret_access_key
 
-  fly_api_token =
-    System.get_env("FLY_API_TOKEN") ||
-      raise """
-      environment variable FLY_API_TOKEN is missing.
-      """
+  # fly_api_token =
+  #   System.get_env("FLY_API_TOKEN") ||
+  #     raise """
+  #     environment variable FLY_API_TOKEN is missing.
+  #     """
 
   config :flame, :backend, FLAME.FlyBackend
 
@@ -234,5 +234,5 @@ if config_env() == :prod do
       "WALLABY_AUTH_PASSWORD" => wallaby_auth_password
     },
     memory_mb: 1024,
-    token: fly_api_token
+    token: System.get_env("FLY_API_TOKEN")
 end
