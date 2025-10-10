174b2cc09720608b05fab11a80f15875c40a3bd0
Dean Kinyua <kinyuadean@gmail.com>
2025-10-10 10:27:11 +0300

    revert those changes

--- priv/repo/migrations/20251010063256_update_drops_with_comment_count.exs
+++
@@ -1,9 +0,0 @@
-defmodule ElixirDrops.Repo.Migrations.UpdateDropsWithCommentCount do
-  use Ecto.Migration
-
-  def change do
-    alter table(:drops) do
-      add :comment_count, :integer
-    end
-  end
-end
