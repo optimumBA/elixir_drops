843762bc30fc643094b81962bfd16bda0a5ec760
Dean Kinyua <kinyuadean@gmail.com>
2025-10-16 15:51:12 +0300

    enhance modal appearance

--- lib/elixir_drops_web/live/comment/comment_form_component.ex
+++ lib/elixir_drops_web/live/comment/comment_form_component.ex
@@ -5,18 +5,18 @@ defmodule ElixirDropsWeb.Comment.CommentFormComponent do
   @impl Phoenix.LiveComponent
   def render(assigns) do
     ~H"""
-    <div class="flex flex-col gap-2">
-      <section class="font-bold">Delete Comment</section>
-      <section>Are you sure you want to delete this comment?</section>
-      <section class="flex gap-4 justify-end">
+    <div class="flex flex-col gap-2 roboto-medium">
+      <section class="text-[#252525] text-2xl">Delete Comment</section>
+      <section class="roboto-regular">Are you sure you want to delete this comment?</section>
+      <section class="flex gap-4 justify-end mt-4">
         <button
-          class="flex items-center gap-x-2 rounded-lg mb-6 px-6 py-4 bg-[#EEEEEE]"
+          class="w-24 h-12 flex justify-center items-center rounded-lg mb-6 text-[#4F4F4F] bg-[#EEEEEE]"
           phx-click={JS.exec("data-cancel", to: "#delete-comment-modal-#{@comment.id}-modal")}
         >
           Cancel
         </button>
         <button
-          class="flex items-center gap-x-2 mb-6 text-[#EAE8FD] px-6 py-4 rounded-lg bg-[#2F19EE]"
+          class="w-24 h-12 flex justify-center items-center rounded-lg mb-6 text-[#EAE8FD] bg-[#2F19EE]"
           phx-click={
             hide_modal("delete-comment-modal-#{@comment.id}-modal")
             |> JS.push("delete_comment", value: %{comment_id: @comment.id, patch_url: @patch})
