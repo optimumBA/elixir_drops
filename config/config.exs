a562e9539aaa35544e584dca37314b37b17d192a
Dean Kinyua <kinyuadean@gmail.com>
2025-10-22 15:34:26 +0300

    migrate to tailwind v4 conventions

--- config/config.exs
+++ config/config.exs
@@ -45,7 +45,7 @@ config :elixir_drops, ElixirDrops.Mailer, adapter: Swoosh.Adapters.Local
 # Configure esbuild (the version is required)
 config :esbuild,
   version: "0.25.4",
-  skeptic_bot: [
+  elixir_drops: [
     args:
       ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
     cd: Path.expand("../assets", __DIR__),
@@ -55,7 +55,7 @@ config :esbuild,
 # Configure tailwind (the version is required)
 config :tailwind,
   version: "4.1.7",
-  skeptic_bot: [
+  elixir_drops: [
     args: ~w(
       --input=assets/css/app.css
       --output=priv/static/assets/css/app.css
