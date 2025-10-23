4b8aac9ff544dc79ede157a4df26b954030b5a33
Dean Kinyua <kinyuadean@gmail.com>
2025-10-23 13:24:55 +0300

    remove fly_api_token

--- config/runtime.exs
+++ config/runtime.exs
@@ -214,12 +214,6 @@ if config_env() == :prod do
     region: aws_region,
     secret_access_key: aws_secret_access_key
 
-  fly_api_token =
-    System.get_env("FLY_API_TOKEN") ||
-      raise """
-      environment variable FLY_API_TOKEN is missing.
-      """
-
   config :flame, :backend, FLAME.FlyBackend
 
   config :flame, FLAME.FlyBackend,
@@ -236,5 +230,5 @@ if config_env() == :prod do
       "WALLABY_AUTH_PASSWORD" => wallaby_auth_password
     },
     memory_mb: 1024,
-    token: fly_api_token
+    token: System.get_env("FLY_API_TOKEN")
 end
