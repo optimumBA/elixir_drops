08038123a07fe62dd53db57abd94c0066f1301b9
Dean Kinyua <kinyuadean@gmail.com>
2025-11-28 14:35:06 +0300

    create notifications schema and context

---
+++ lib/elixir_drops/notifications/notification.ex
@@ -0,0 +1,31 @@
+defmodule ElixirDrops.Notifications.Notification do
+  @moduledoc false
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias ElixirDrops.Accounts.User
+  alias ElixirDrops.Drops.Drop
+
+  @type t :: %__MODULE__{}
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "notifications" do
+    field :read, :boolean, default: false
+    field :type, Ecto.Enum, values: [:comment_on_post, :reply_to_comment]
+
+    belongs_to :actor, User
+    belongs_to :drop, Drop
+    belongs_to :recipient, User
+
+    timestamps()
+  end
+
+  @spec changeset(t(), map()) :: Ecto.Changeset.t()
+  def changeset(notification, attrs) do
+    notification
+    |> cast(attrs, [:read, :type])
+    |> validate_required([:read, :type])
+  end
+end
